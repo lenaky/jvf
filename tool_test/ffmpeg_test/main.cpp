2926e81fa5291e4ad7cf9fa22dd4474a3ff3beba
lenaky <jaymuzie@gmail.com>
2020-02-26 22:06:39 +0900

    [jvf] rtsp 1 frame 수신받아 decode 후 yuv420p 로 출력

--- tool_test/ffmpeg_test/main.cpp
+++ tool_test/ffmpeg_test/main.cpp
@@ -10,6 +10,7 @@ extern "C"
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
 #include <libavformat/avio.h>
+#include <libavutil/imgutils.h>
 
 #pragma comment(lib, "avcodec.lib")
 #pragma comment(lib, "avformat.lib")
@@ -95,7 +96,7 @@ void TestRTSPClient()
         return;
     }
 
-    unsigned int limit_packet = 30; // 30 pictures
+    unsigned int limit_packet = 3000; // 30 pictures
 
     av_init_packet( pkt );
     while( av_read_frame( fmt_ctx, pkt ) >= 0 && --limit_packet > 0)
@@ -107,21 +108,46 @@ void TestRTSPClient()
             if( 0 > ret )
             {
                 LOG_E( "send packet failed. ret={}", ret );
-                break;
+                continue;
             }
-
+            LOG_I( "send packet success" );
             while( 0 <= ret )
             {
                 ret = avcodec_receive_frame( v_dec_ctx, av_frm );
                 if( ret == AVERROR( EAGAIN ) || ret == AVERROR_EOF )
                 {
-                    LOG_E( "error code={}", ret );
-                    return;
+                    continue;
                 }
                 else if( ret < 0 )
                 {
                     LOG_E( "decode failed!" );
                     return;
+                }
+
+                if( v_dec_ctx->width > 0 && v_dec_ctx->height > 0 )
+                {
+                    auto pix_fmt = AV_PIX_FMT_BGR24;
+
+                    auto pic_size = av_image_get_buffer_size( v_dec_ctx->pix_fmt, 
+                                                              v_dec_ctx->width, 
+                                                              v_dec_ctx->height, 
+                                                              16 );
+                    uint8_t* buffer = (uint8_t*)av_malloc( pic_size );
+                    auto number_of_written_bytes = av_image_copy_to_buffer( buffer, pic_size,
+                                                                            av_frm->data, av_frm->linesize,
+                                                                            v_dec_ctx->pix_fmt, v_dec_ctx->width, v_dec_ctx->height, 1 );
+                    std::ofstream ofs( "./test.yuv", std::ios::binary );
+                    if( ofs )
+                    {
+                        ofs.write( (const char*)buffer, number_of_written_bytes );
+                        ofs.close();
+                    }
+
+                    av_free(buffer);
+                    if( number_of_written_bytes < 0 )
+                    {
+                        LOG_E( "could not copy image to buffer" );
+                    }
                 }
 
                 LOG_I( "decode success!" );
